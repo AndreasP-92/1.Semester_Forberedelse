584cad004e9171c4ba9677ad3db412558d1564c5
Andreas Pedersen <31003640+AndreasP-92@users.noreply.github.com>
2020-06-07 16:08:21 +0200

    asd

---
+++ src/com/company/main03.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.*;
+
+public class main03 {
+    public static void main(String[] args){
+        ArrayList<Integer> arr = {22,11};
+
+        System.out.println(arr);
+    }
+}
